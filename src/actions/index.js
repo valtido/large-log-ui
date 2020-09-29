16c679c25dd6fb41d6e089b65091a7f34b791e0b
Valtid Caushi <valtido@hotmail.com>
2020-09-29 17:45:18 +0100

    test test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,18 @@
+const axios = require("axios");
+
 const url = "http://localhost:8080";
 
 export const fetchLogs = async (dispatch, state) => {
   dispatch({ type: "LOADING_DATA" });
 
   try {
-    const res = await fetch(`${url}/logs?start=0&end=2`);
+    const res = await axios.get(`${url}/logs?start=0&end=2`);
+    axios.get(`${url}/logs`, {
+      params: {
+        start: 0,
+        end: 1
+      }
+    });
     const payload = await res.json();
     console.log(payload);
 
